586831c6b9fdf5b3652cde2e061c9d026db9e5b4
Axzyz <ronald.urrutia.vera@gmail.com>
2025-03-18 22:05:56 -0300

    Componente number_selector
    Se sube componente personalizado number_selector y se crean en el menu

---
+++ lib/components/number_selector.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:imc_calculator/core/app_colors.dart';
+import 'package:imc_calculator/core/text_styles.dart';
+
+class NumberSelector extends StatefulWidget {
+  final String title;
+  final int value;
+  final Function() onDecrement;
+  final Function() onIncrement;
+
+  const NumberSelector({super.key, required this.title, required this.value, required this.onDecrement, required this.onIncrement});
+
+  @override
+  State<NumberSelector> createState() => _NumberSelectorState();
+}
+
+class _NumberSelectorState extends State<NumberSelector> {
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        padding: EdgeInsets.all(16.0),
+        decoration: BoxDecoration(
+          color: AppColors.backgroundComponent,
+          borderRadius: BorderRadius.circular(16.0),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(widget.title.toUpperCase(), style: TextStyles.bodyText),
+            Text('${widget.value}', style: TextStyles.componentText),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              spacing: 16.0,
+              children: [
+                FloatingActionButton(
+                  shape: CircleBorder(),
+                  onPressed: () {
+                    widget.onDecrement();
+                  },
+                  backgroundColor: AppColors.primary,
+                  foregroundColor: Colors.white,
+                  child: Icon(Icons.remove),
+                ),
+                FloatingActionButton(
+                  shape: CircleBorder(),
+                  onPressed: () {
+                    widget.onIncrement();
+                  },
+                  backgroundColor: AppColors.primary,
+                  foregroundColor: Colors.white,
+                  child: Icon(Icons.add),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/imc_home_screen.dart
+++ lib/screens/imc_home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:imc_calculator/components/gender_selector.dart';
 import 'package:imc_calculator/components/height_selector.dart';
+import 'package:imc_calculator/components/number_selector.dart';
 
 class ImcHomeScreen extends StatefulWidget {
   const ImcHomeScreen({super.key});
@@ -10,12 +11,59 @@ class ImcHomeScreen extends StatefulWidget {
 }
 
 class _ImcHomeScreenState extends State<ImcHomeScreen> {
+  int selectedAge = 18;
+  int selectedWeight = 80;
+
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         GenderSelector(),
-        HeightSelector()
+        HeightSelector(),
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Row(
+            children: [
+              NumberSelector(
+                title: 'Peso',
+                value: selectedWeight,
+                onDecrement: () {
+                  setState(() {
+                    if (selectedWeight > 20) {
+                      selectedWeight--;
+                    }
+                  });
+                },
+                onIncrement: () {
+                  setState(() {
+                    if (selectedWeight < 300) {
+                      selectedWeight++;
+                    }
+                  });
+                },
+              ),
+              SizedBox(width: 16.0),
+              NumberSelector(
+                title: 'Edad',
+                value: selectedAge,
+                onDecrement: () {
+                  setState(() {
+                    if (selectedAge > 5) {
+                      selectedAge--;
+                    }
+                  });
+                },
+                onIncrement: () {
+                  setState(() {
+                    if (selectedAge < 100) {
+                      selectedAge++;
+                    }
+                  });
+                },
+              ),
+            ],
+          ),
+        ),
       ],
     );
   }
